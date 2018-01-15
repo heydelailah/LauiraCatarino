6e64346bad5a3007c57091021c70d282932d7240
heydelailah <heydelailah@gmail.com>
2018-01-15 16:47:09 -0500

    Mon Jan 15 16:47:09 EST 2018

--- permission.sh
+++ permission.sh
@@ -5,3 +5,11 @@ sudo chown cabox:apache -R /home
 #sudo chown cabox:www-data -R ~/workspace 
 
 #ln -s .git git
+
+#Ubuntu: sudo chown cabox:www-data -R ~/workspace; 
+#CentOS: sudo chown cabox:apache -R ~/workspace
+#-R /home
+
+#eval $(ssh-agent -s)
+
+#ssh-add ~/.ssh/id_rsa
