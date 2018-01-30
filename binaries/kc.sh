6d1c510344738ec923618d5b11c7024a0263f861
delailahSec <heydelailah@gmail.com>
2018-01-30 07:38:24 -0500

    Tue Jan 30 07:38:24 EST 2018

--- binaries/kc.sh
+++ binaries/kc.sh
@@ -1,6 +1,8 @@
 #install keychain
 #writen alexsi rebou135as
-rpm -i keychain-2.8.0-3.el7.psychotic.noarch.rpm
+
+#rpm -i keychain-2.8.0-3.el7.psychotic.noarch.rpm
+#dpkg -i keychain_2.7.1-1_all.deb
 echo "/usr/bin/keychain $HOME/.ssh/id_rsa" >> ~/.bashrc
 echo "source $HOME/.keychain/$HOSTNAME-sh" >> ~/.bashrc
 #or
