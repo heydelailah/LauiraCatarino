8aab77151fd435de8c27f189e11e394a6b11533a
root <root@box-codeanywhere.com>
2018-01-30 08:50:13 -0500

    Tue Jan 30 08:50:13 EST 2018

--- binaries/kc.sh
+++ binaries/kc.sh
@@ -2,7 +2,7 @@
 #writen alexsi rebou135as
 
 #rpm -i keychain-2.8.0-3.el7.psychotic.noarch.rpm
-#dpkg -i keychain_2.7.1-1_all.deb
+dpkg -i keychain_2.7.1-1_all.deb
 echo "/usr/bin/keychain $HOME/.ssh/id_rsa" >> ~/.bashrc
 echo "source $HOME/.keychain/$HOSTNAME-sh" >> ~/.bashrc
 #or
